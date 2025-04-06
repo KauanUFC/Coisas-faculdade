39d38dbe6ea32f1be01f7d8380911698adf022d8
KauanUFC <hawkkauan@gmail.com>
2025-04-05 22:02:50 -0300

    Update o server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
-import express from 'express';
-import cors from 'cors';
-import path from 'path';
-import { fileURLToPath } from 'url';
+import express from "express";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -14,19 +14,17 @@ app.use(cors());
 app.use(express.json()); // Use Express's built-in JSON middleware
 
 // Serve the HTML file
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html')); // Serve the HTML file at the root route
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "index.html")); // Serve the HTML file at the root route
 });
 
 // Handle POST request
-app.post('/input', (req, res) => {
-    const { input } = req.body; // Extract input from the request body
-    console.log(input); // Log the input value to the console
-    res.send("Input received"); // Send a response back to the client
-
+app.post("/input", (req, res) => {
+  const { input } = req.body; // Extract input from the request body
+  console.log(input); // Log the input value to the console
+  res.send("Input received"); // Send a response back to the client
 });
 
-
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
+  console.log(`Server: http://localhost:${PORT}`);
 });
